f4ef6eb22295f6e1d34784b10f6ad237dcf84b70
locusxt <locusxt@163.com>
2015-07-04 20:49:26 +0800

    add sudoku generator

--- my_sudoku/sudoku/urls.py
+++ my_sudoku/sudoku/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
                         url(r'^ajax/add_game_log/$', views.add_game_log),
                         url(r'^history/$', views.show_history, name='history'),
                         url(r'^history/ajax/get_history/$', views.get_history),
+                        url(r'^ajax/get_new_game/$', views.get_new_sudoku),
 )
--- my_sudoku/sudoku/views.py
+++ my_sudoku/sudoku/views.py
@@ -8,6 +8,8 @@ from django.http import HttpResponse
 
 from sudoku.models import GameLog
 import datetime
+import sys
+from Sudoku.Generator import *
 
 # Create your views here.
 def start_sudoku(request):
@@ -53,27 +55,32 @@ def get_history(request):
 
 
 
+difficulties = {
+    'easy': (35, 0), 
+    'medium': (81, 5), 
+    'hard': (81, 10), 
+    'extreme': (81, 15)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@csrf_exempt
+def get_new_sudoku(request):
+	if not request.is_ajax():
+		return HttpResponse("Not Ajax")
+	if request.method != 'POST':
+		return HttpResponse("Not Ajax Post")
+	d = json.loads(request.body)
+	print(d)
+	difficulty = difficulties[d['level']]
+	gen = Generator()
+	gen.randomize(100)
+	initial = gen.board.copy()
+	gen.reduce_via_logical(difficulty[0])
+	if difficulty[1] != 0:
+		gen.reduce_via_random(difficulty[1])
+	final = gen.board.copy()
+	#print(final)
+	response_data = {'sudoku' : str(final)}
+	return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
 
